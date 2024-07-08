ad37959a0eac6f4a01153687760de5ae66c0c3a0
Puja Patel <ppate343@uwo.ca>
2024-07-08 12:32:41 -0400

    added routes in app.js

--- cashly-app/src/App.js
+++ cashly-app/src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
+import Navbar from './components/Navbar';
+import UserDetails from './pages/UserDetails';
+import Calculators from './pages/Calculators';
+import LenderQualifier from './pages/LenderQualifier';
+import Notes from './pages/Notes';
+import CashlyCopilot from './pages/CashlyCopilot';
+import TechServices from './pages/TechServices';
+import Resources from './pages/Resources';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+    <CssBaseline />
+    <Navbar />
+    <Switch>
+      <Route path="/user-details" component={UserDetails} />
+      <Route path="/calculators" component={Calculators} />
+      <Route path="/lender-qualifier" component={LenderQualifier} />
+      <Route path="/notes" component={Notes} />
+      <Route path="/cashly-copilot" component={CashlyCopilot} />
+      <Route path="/tech-services" component={TechServices} />
+      <Route path="/resources" component={Resources} />
+      <Route path="/" exact component={UserDetails} />
+    </Switch>
+  </Router>
+);
 }
 
 export default App;
